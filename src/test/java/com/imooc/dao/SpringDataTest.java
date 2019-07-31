4a7bc50a7589fe1a53881de7d28c99808c2c7300
king <419562071@qq.com>
2019-07-31 15:08:00 +0800

    spring-data 简单使用

---
+++ src/test/java/com/imooc/dao/SpringDataTest.java
@@ -0,0 +1,56 @@
+package com.imooc.dao;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
+
+//import static org.junit.Assert.*;
+
+public class SpringDataTest {
+    private ApplicationContext ctx = null;
+    // private StudentDAO studentDAO = null;
+
+    @Before
+    public void setup() {
+        ctx = new ClassPathXmlApplicationContext("beans.xml");
+        // studentDAO = (StudentDAO)ctx.getBean("studentDAO");
+        System.out.println("setup");
+    }
+
+    @After
+    public void tearDown() {
+        ctx = null;
+        System.out.println("tearDown");
+    }
+
+    //运行这个生成表格
+    @Test
+    public void testEntityManagerFactory() {
+
+    }
+
+/*    @Test
+    public void testQuery() {
+        List<Student> students = studentDAO.query();
+
+        for (Student student : students) {
+            System.out.println("id:" + student.getId()
+                    + " , name:" + student.getName()
+                    + ", age:" + student.getAge());
+        }
+
+    }
+
+    @Test
+    public void testSave() {
+        Student student = new Student();
+        student.setName("test-spring-jdbc");
+        student.setAge(40);
+
+        studentDAO.save(student);
+    }*/
+}
---
+++ src/test/java/com/imooc/repository/EmployeeRepositoryTest.java
@@ -0,0 +1,100 @@
+package com.imooc.repository;
+
+import com.imooc.domain.entity.Employee;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class EmployeeRepositoryTest {
+
+    private ApplicationContext ctx = null;
+    private EmployeeRepository employeeRepository = null;
+
+    @Before
+    public void setup() {
+        ctx = new ClassPathXmlApplicationContext("beans.xml");
+        employeeRepository = ctx.getBean(EmployeeRepository.class);
+
+        // studentDAO = (StudentDAO)ctx.getBean("studentDAO");
+        System.out.println("setup");
+    }
+
+    @After
+    public void tearDown() {
+        ctx = null;
+        System.out.println("tearDown");
+    }
+
+    @Test
+    public void testFindByName() { //throws Exception {
+        Employee employee = employeeRepository.findByName("yjd");
+
+        if (employee != null) {
+            System.out.println("id: " + employee.getId() + "name: " + employee.getName() + "age: " + employee.getAge());
+        }
+    }
+
+    @Test
+    public void findByNameInOrAgeLessThan() {
+        ArrayList<String> nameList = new ArrayList<>();
+
+        nameList.add("test1");
+        nameList.add("test2");
+        nameList.add("test3");
+
+        List<Employee> employeeList = employeeRepository.findByNameInOrAgeLessThan(nameList, 21);
+        if (employeeList != null) {
+            for (Employee employeeTmp : employeeList) {
+                System.out.println("id: " + employeeTmp.getId() + " , name: " + employeeTmp.getName() + " , age: " + employeeTmp.getAge());
+            }
+        }
+    }
+
+    @Test
+    public void testGetEmployeeByMaxId() {
+        Employee employee = employeeRepository.getEmployeeByMaxId();
+
+        if (employee != null) {
+            System.out.println("id: " + employee.getId() + "name: " + employee.getName() + "age: " + employee.getAge());
+        }
+    }
+
+    @Test
+    public void queryParams1() { //throws Exception {
+        List<Employee> employeeList = employeeRepository.queryParams1("test1", 20);
+
+        if (employeeList != null) {
+            for (Employee employeeTmp : employeeList) {
+                System.out.println("id: " + employeeTmp.getId() + " , name: " + employeeTmp.getName() + " , age: " + employeeTmp.getAge());
+            }
+        }
+    }
+
+    @Test
+    public void queryLike1() { //throws Exception {
+        List<Employee> employeeList = employeeRepository.queryLike1("test");
+
+        if (employeeList != null) {
+            for (Employee employeeTmp : employeeList) {
+                System.out.println("id: " + employeeTmp.getId() + " , name: " + employeeTmp.getName() + " , age: " + employeeTmp.getAge());
+            }
+        }
+    }
+    //错误的实例
+    /*@Test
+    public void findName() { //throws Exception {
+        Employee employee = employeeRepository.findName("yjd");
+
+        if(employee != null) {
+            System.out.println("id: " + employee.getId() + "name: " + employee.getName() + "age: " + employee.getAge());
+        }
+    }*/
+}

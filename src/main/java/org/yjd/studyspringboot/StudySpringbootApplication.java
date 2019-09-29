320ee9965d58ad4f095e211e2d6b2a915122cd3b
yjdking <419562071@qq.com>
2019-09-29 23:09:09 +0800

    配置swagger

--- src/main/java/org/yjd/studyspringboot/StudySpringbootApplication.java
+++ src/main/java/org/yjd/studyspringboot/StudySpringbootApplication.java
@@ -2,8 +2,18 @@ package org.yjd.studyspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+
+// 引入swagger
+// 通过@Configuration注解，让Spring来加载该类配置。再通过@EnableSwagger2注解来启用Swagger2。
+@Configuration  // //让Spring来加载该类配置
+@EnableSwagger2
+
+//启用Mvc，非springboot框架需要引入注解@EnableWebMvc
+// @EnableWebMvc
 public class StudySpringbootApplication {
 
 	public static void main(String[] args) {

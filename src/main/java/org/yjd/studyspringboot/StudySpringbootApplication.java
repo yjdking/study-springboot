f20ddaf692bfac001b2998699bc1c2992b28cf79
king <419562071@qq.com>
2019-10-10 14:52:41 +0800

    jpa搭建完成

--- src/main/java/org/yjd/studyspringboot/StudySpringbootApplication.java
+++ src/main/java/org/yjd/studyspringboot/StudySpringbootApplication.java
@@ -2,16 +2,12 @@ package org.yjd.studyspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 // 包含了@ComponentScan({"org.yjd.studyspringboot"})
 @SpringBootApplication
-// @EnableJpaRepositories("org.yjd.studyspringboot.infrastructure.persistence.hibernate")
-//  扫描@Repository
-@EnableJpaRepositories(basePackages = "org.yjd.studyspringboot.infrastructure.persistence.hibernate")
+
+//  扫描@Repository, 这个加不加都可以
+// @EnableJpaRepositories()
 
 //启用Mvc，非springboot框架需要引入注解@EnableWebMvc
 // @EnableWebMvc

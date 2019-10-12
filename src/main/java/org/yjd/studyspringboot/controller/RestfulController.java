a1c188082d4296cc2995dd8ff0125a36090a9884
king <419562071@qq.com>
2019-10-12 12:01:15 +0800

    few request params is null

--- src/main/java/org/yjd/studyspringboot/controller/RestfulController.java
+++ src/main/java/org/yjd/studyspringboot/controller/RestfulController.java
@@ -4,8 +4,10 @@ import com.alibaba.fastjson.JSON;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.yjd.studyspringboot.controller.dto.RestfulRequest;
 
@@ -13,17 +15,18 @@ import org.yjd.studyspringboot.controller.dto.RestfulRequest;
 // @RequestMapping("/restful")
 public class RestfulController {
 
-    @GetMapping("/pathvariable/{id}")
-    public String test(@PathVariable String id){
-        return "SUCCESS: id, " + id;
+    @GetMapping(value = "/pathvariable/{id}")
+    public String test(@PathVariable String id, Integer test){
+        return "SUCCESS: id, " + id + "test, " + test;
     }
 
     @GetMapping("/requestparam/value")
-    public String testRequestParamRestful(@RequestParam String name, int id){
+    public String testRequestParamRestful(@RequestParam String name, Integer id){
         return  "Restful successed: " + "name: " +  name +  ",id: " + id;
     }
 
-       @GetMapping("/requestparam/object")
+    // 这种方式无法获得对象
+    @GetMapping("/requestparam/object")
     public String testRequestParamRestful(@RequestParam RestfulRequest userRestfulRequest){
         return  "Restful successed: " + JSON.toJSONString(userRestfulRequest);
     }
@@ -38,8 +41,9 @@ public class RestfulController {
         return "SUCCESS: id, " + id;
     }
 
-    @PutMapping("/requestparam/value")
-    public String putTestRequestParam(@RequestParam String name, int id){
-        return  "Restful successed: " + "name: " +  name +  ",id: " + id;
+    @PutMapping("/requestbody")
+    public String putTestRequestParam(@RequestBody RestfulRequest userRestfulRequest){
+        return  "Restful successed: " + JSON.toJSONString(userRestfulRequest);
     }
+
 }
